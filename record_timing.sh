ecde8223eff451a6ff9c7f43c178fd5329ce47c8
annoyatron255 <annoyatron255@gmail.com>
2019-01-11 07:58:33 -0600

    Update base data collection script

--- record_timing.sh
+++ record_timing.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-icetime -tmd hx8k -p top.pcf -P tq144 $(find *.asc | head -n 1) | grep -o -P '(?<=\().*(?=MHz)' | tr ' ' ',' >> timing.csv
+for i in `seq 1 10`;
+do
+	make clean
+	make
+	icetime -tmd hx8k -p $(find *.pcf | head -n 1) -P tq144 $(find *.asc | head -n 1) | grep -o -P '(?<=\().*(?=MHz)' | tr ' ' ',' >> timing.csv
+done
